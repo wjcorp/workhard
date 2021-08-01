220890b4ae16f0ba76f18176a3addcb5c8009798
yjcorp <88293266+wjcorp@users.noreply.github.com>
2021-08-02 07:35:52 +0900

    Add files via upload

---
+++ yjcorpProdList.js
@@ -0,0 +1,12 @@
+var prodList = [
+    { seq: 1, name: '호스', imgsrc: 'https://i.ibb.co/fNs4H93/image.jpg', desc1:'15mm', desc2:'30미터/50미터', rdate:'2020-07-15', isNew:1, cate:1 },
+    { seq: 2, name: '호스', imgsrc: 'https://i.ibb.co/bRrHZTm/image.jpg', desc1:'15mm', desc2:'30미터/50미터', rdate:'2021-02-03', isNew:0, cate:1 },
+    { seq: 3, name: '편사', imgsrc: 'https://i.ibb.co/zrNZ6zK/image.jpg', desc1:'16mm', desc2:'40미터/45미터', rdate:'2020-10-21', isNew:0, cate:2 },
+    { seq: 4, name: '호스릴', imgsrc: 'https://i.ibb.co/XFgp5fJ/image.jpg', desc1:'15mm', desc2:'30미터/50미터', rdate:'2021-01-01', isNew:0, cate:2 },
+    { seq: 5, name: '가든', imgsrc: 'https://i.ibb.co/XFgp5fJ/image.jpg', desc1:'15mm', desc2:'30미터/50미터', rdate:'2021-05-01', isNew:1, cate:3 },
+    { seq: 6, name: '가든', imgsrc: 'https://i.ibb.co/XFgp5fJ/image.jpg', desc1:'15mm', desc2:'30미터/50미터', rdate:'2021-05-01', isNew:1, cate:3 },
+    { seq: 7, name: '가든', imgsrc: 'https://i.ibb.co/XFgp5fJ/image.jpg', desc1:'15mm', desc2:'30미터/50미터', rdate:'2021-05-01', isNew:1, cate:3 },
+    { seq: 8, name: '가든', imgsrc: 'https://i.ibb.co/XFgp5fJ/image.jpg', desc1:'15mm', desc2:'30미터/50미터', rdate:'2021-05-01', isNew:1, cate:3 },
+    { seq: 9, name: '석유통', imgsrc: 'https://i.ibb.co/XFgp5fJ/image.jpg', desc1:'15mm', desc2:'30미터/50미터', rdate:'2021-05-01', isNew:1, cate:3 },
+    { seq: 10, name: '분사기', imgsrc: 'https://i.ibb.co/XFgp5fJ/image.jpg', desc1:'15mm', desc2:'30미터/50미터', rdate:'2021-05-01', isNew:1, cate:4 }
+];
